35425b58158a19d0780273e31ea61892445d6b47
melissa12 <melissawhiting11@googlemail.com>
2017-03-02 10:29:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,20 @@ app.post('/webhook', function (req, res) {
 	res.sendStatus(200);
 });
 
+function sendMessage(recipientID, message) { 
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+		method: 'POST',
+		json: {
+			recipient: {id: recipientID},
+			message: message, 
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+	});
+};
