b0fd1ff7c3004c8107e40ac68b1e539965cda524
melissa12 <melissawhiting11@googlemail.com>
2017-03-02 15:28:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text === 'hello there') {
-            sendMessage(event.sender.id, {text: 'hi'});
+        if (event.message && event.message.text === 'Hello there') {
+            sendMessage(event.sender.id, {text: "hi"});
         }
     }
     res.sendStatus(200);
