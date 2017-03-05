479cfd2bc4902ab2841a76e8739560043ce57245
melissa12 <melissawhiting11@googlemail.com>
2017-03-05 16:28:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');  
 var request = require('request');  
 var app = express();
-var MessengerPlatform = require('facebook-bot-messenger');
 
 app.use(bodyParser.urlencoded({extended: false}));  
 app.use(bodyParser.json());  
