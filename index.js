807412c12b53312f37a78684324ef62d5a6db73f
melissa12 <melissawhiting11@googlemail.com>
2017-03-05 16:07:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.post('/webhook', function (req, res) {
         }
         else if (event.message && event.message.text === "Hello"){
             sendMessage(event.sender.id, {text: "Blah"}); 
+        }
     }
     res.sendStatus(200);
 });
