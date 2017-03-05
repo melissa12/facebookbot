af0bc5cf97222d035a8a4d4395dab09230d7ea18
melissa12 <melissawhiting11@googlemail.com>
2017-03-05 20:13:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');  
 var request = require('request');  
 var app = express();
-var builder = require('botbuilder');
 
 app.use(bodyParser.urlencoded({extended: false}));  
 app.use(bodyParser.json());  
