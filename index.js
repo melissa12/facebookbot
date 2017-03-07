5f4093623051ac1ad95fc74660c93f60da01584d
melissa12 <melissawhiting11@googlemail.com>
2017-03-07 11:47:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,30 @@ app.post('/webhook', function (req, res) {
             imageMessage(event.sender.id, {text: "Here"});
             imageMessage(event.sender.id, {text: "Here"});
         }
+        else if (event.message && event.message.text === "I am at the canteen and I want to go to the Toilet") { 
+            canteenToilets(event.sender.id, {text: "Here"});
+        }
+        else if (event.message && event.message.text === "I am at the canteen and I want to go to T2.07") { 
+            canteenT207(event.sender.id, {text: "Here"});
+        }
+        else if (event.message && event.message.text === "I am at the canteen and I want to go to the Library") { 
+            canteenLibrary(event.sender.id, {text: "Here"});
+        }
+        else if (event.message && event.message.text === "I am at the canteen and I want to go to T2.09") { 
+            canteenT209(event.sender.id, {text: "Here"});
+        }
+        else if (event.message && event.message.text === "I am at the canteen and I want to go to C2.04") { 
+            canteenC204_1(event.sender.id, {text: "Here"});
+            canteenC204_2(event.sender.id, {text: "Here"}); 
+        }
+        else if (event.message && event.message.text === "I am at the canteen and I want to go to C2.07") { 
+            canteenC207_1(event.sender.id, {text: "Here"});
+            canteenC207_2(event.sender.id, {text: "Here"}); 
+        }
+        else if (event.message && event.message.text === "I am at the canteen and I want to go to S1.32") { 
+            canteenS132_1(event.sender.id, {text: "Here"});
+            canteenS132_2(event.sender.id, {text: "Here"}); 
+        }
     }
     res.sendStatus(200);
 });
@@ -94,3 +118,243 @@ function imageMessage(recipientId, text) {
 };
 
 
+function canteenToilets(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Walk towards the JCR and turn right. The toilets are just on your left."
+                     }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+
+function canteenT207(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Take the stairs that are in front of you. Go up 4 flights." 
+                     },
+                    {
+                        "subtitle": "Go through the door in front of you."
+                    },
+                    {
+                        "subtitle": "Walk along the corridor."
+                    },
+                    {
+                        "subtitle": "It's the second door on your right."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+function canteenLibrary(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Take the stairs that are in front of you. Go up 2 flights." 
+                     },
+                    {
+                        "subtitle": "Go through the door to the right of you. This is the library."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+function canteenT209(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Take the stairs that are in front of you. Go up 4 flights." 
+                     },
+                    {
+                        "subtitle": "T2.09 is just to your right."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+
+function canteenC204_1(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Take the stairs that are in front of you. Go up 3 flights." 
+                     },
+                    {
+                        "subtitle": "Turn right and walk along the corridor."
+                    },
+                    {
+                        "subtitle": "At the end of the corridor go up 2 flights of stairs."
+                    },
+                    {
+                        "subtitle": "Turn right and right again."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+
+function canteenC204_2(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Walk along this corridor." 
+                     },
+                    {
+                        "subtitle": "It's the fourth door on your left."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+function canteenC207_1(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Take the stairs that are just in front of you. Go up 3 flights." 
+                     },
+                    {
+                        "subtitle": "Turn right and walk along the corridor."
+                    },
+                    {
+                        "subtitle": "At the end of the corridor go up 2 flights of stairs."
+                    },
+                    {
+                        "subtitle": "Turn right and right again."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+function canteenC207_2(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Walk along this corridor." 
+                     },
+                    {
+                        "subtitle": "It's the seventh door on your left."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+function canteenS132_1(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "Take the stairs that are just in front of you. Go up 3 flights." 
+                     },
+                    {
+                        "subtitle": "Turn right and walk along the corridor."
+                    },
+                    {
+                        "subtitle": "At the end of the corridor go up 2 flights of stairs."
+                    },
+                    {
+                        "subtitle": "Turn right and right again."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
+
+
+function canteenS132_2(recipientId, text) { 
+    message = { 
+        "attachment": {
+            "type": "template",
+            "payload": { 
+                "template_type": "list", 
+                "top_element_style": "compact",
+                "elements": [
+                    {
+                    "subtitle": "At the end of this corridor turn right." 
+                     },
+                    {
+                        "subtitle": "Turn left and go up 1 flight of stairs."
+                    },
+                    {
+                        "subtitle": "Go down the stairs until you reach the first floor."
+                    },
+                    {
+                        "subtitle": "Walk along this corridor and take the second right. The door is just on your right."
+                    }
+                ]
+            }
+        }
+    };
+    sendMessage(recipientId, message);
+};
