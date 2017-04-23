ee5c7f0a5ac49838c1ae72b13757b5183922d1d9
melissa12 <melissawhiting11@googlemail.com>
2017-04-23 12:08:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -327,6 +327,9 @@ app.post('/webhook', function (req, res) {
         else if (event.message && event.message.text === "I am at S1.32 and I want to go to S2.22") { 
             S132S222(event.sender.id, {text: "Here"}); 
         }
+        else if (event.message) {
+            sendMessage(event.sender.id, {text: "I'm sorry, I don't understand! Type 'help to learn how to interact with me."}); 
+        }
             
         
     }
